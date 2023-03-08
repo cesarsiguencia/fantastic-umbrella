61119a5246c03efc52cf225fc0ec8ee38e64d46d
cesarsiguencia <cesar.siguencia@gmail.com>
2023-03-08 15:01:42 -0500

    removed comments and personal MYSQL credentials for production

--- models/index.js
+++ models/index.js
@@ -4,18 +4,14 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreign_key: 'category_id'
 })
 
-// Categories have many Products
 Category.hasMany(Product, {
   foreign_key: 'category_id'
 })
 
-//STUDENT NOTE: THESE ASSOCIATIONS WERE MISSING FROM THE ASSIGNMENT INSTRUCTIONS 
-// =====================================
 Product.hasMany(ProductTag, {
   foreignKey: 'product_id'
 });
@@ -31,24 +27,18 @@ Tag.hasMany(ProductTag, {
 ProductTag.belongsTo(Tag, {
   foreignKey: 'tag_id'
 })
-//=======================================
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'associated_tags',
   foreignKey: 'product_id'
 })
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'associated_products',
   foreignKey: 'tag_id'
 })
-
-
-
 
 
 
@@ -58,8 +48,3 @@ module.exports = {
   Tag,
   ProductTag,
 };
-
-
-
-
-// product_name has tags
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   Category.findAll({
     attributes: ['id','category_name'],
     include: {
@@ -23,8 +20,6 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     attributes: ['id','category_name'],
     where: {
@@ -44,7 +39,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
   Category.create({
     category_name: req.body.category_name
   })
@@ -56,7 +50,6 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   Category.update({
     category_name: req.body.category_name
     },
@@ -80,7 +73,6 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   Category.destroy({
     where: {
       id: req.params.id
